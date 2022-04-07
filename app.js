9b68515eacf8b3af496e15220bb5ff2e1bff98b9
Martin-Salomon <martinsalomon11@gmail.com>
2022-04-06 21:28:31 -0300

    .

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ app.get('/register', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
+
+app.post('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
